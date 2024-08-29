277cf6ffeeb31a6246fab447a0afba22ce1f74b4
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-08-29 18:30:07 -0400

    I added code to the NavTabs component

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,1 +1,34 @@
-import { Li}
+import { Link, useLocation } from 'react-router-dom';
+
+function NavTabs() {
+    const currentPage = useLocation().pathname;
+
+    return (
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link 
+                  to="/"
+                  className={currentPage === '/' ? 'nav-link active' : 'nav-link'} >Home</Link>
+                  </li>
+            <li className="nav-item">
+                <Link 
+                 to="/Portfolio"
+                 className={currentPage === '/' ? 'nav-link active' : 'nav-link'} >Portfolio</Link>
+                 </li>
+            <li className="nav-item">
+                <Link 
+                 to="/Resume"
+                 className={currentPage === '/' ? 'nav-link active' : 'nav-link'} >Resume</Link>
+                 </li>
+                 <li className="nav-item">
+            <Link 
+                 to="/Contact"
+                 className={currentPage === '/' ? 'nav-link active' : 'nav-link'} >Contact</Link>
+                 </li>
+        </ul>
+    );
+}
+
+
+
+export default NavTabs;
