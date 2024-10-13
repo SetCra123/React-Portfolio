e17ae11cffe3eb2c887b769bf9dd642e26f69487
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-10-13 02:03:54 -0400

    I added attributes to div and children on Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 export default function Home() {
     return (
-        <div>
-            <h1>About Me</h1>
-            <p>I am a developer from Brooklyn, NY, who used to be a teacher and a Journalist. I taught
+        <div className="">
+            <h1 className="">About Me</h1>
+            <p className="">I am a developer from Brooklyn, NY, who used to be a teacher and a Journalist. I taught
                 middle-school Math and Coding. enjoy building apps, shooting video and 
                 playing sports. Through my Content Creation agency, Culture Media, I've shot 
                 everything from sports to events to commercials. I also played semi-pro football in New York City for a decade.
                 I also love flying my drone and getting epic aerial footage. I spend alot 
                 of time with my kids, Amara and Setrige III. I love reading, excercising, watching anime
                 and doing fun, silly activities with my kids. </p>
-        <img src="./images/IMG_5121.png" max-height="200px" ></img>
+        <img src="./images/IMG_5121.png" max-height="200px" className=""></img>
         
         </div>
         
