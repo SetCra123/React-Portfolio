ed74787eaf6450006f81604c12870f310885d73a
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-10-14 00:38:47 -0400

    I updated the css for the main content

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ import { useEffect } from 'react';
 function App() {
   useEffect(() => {
     NET({
-      el: '#vanta'
+      el: '#vanta',
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0xa78721,
+      backgroundColor: 0xf0e11
     })
   }, [])
   
@@ -14,12 +23,15 @@ function App() {
 
   return (
     <><div className="app">
-      <div className="bg" id="vanta"></div>
-    </div><>
-        <Nav />
+      <div className="bg" id="vanta">
+      <Nav />
         <main className="mx-3">
           <Outlet />
         </main>
+
+      </div>
+    </div><>
+        
       </></>
   );
 }
