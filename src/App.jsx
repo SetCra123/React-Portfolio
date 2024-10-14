67652214a762b1d412d1ca431e55a9345e51beb6
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-10-14 00:05:02 -0400

    I installed vanta, three.js, and added code for animated bg in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
+import NET from 'vanta/src/vanta.net';
+import { useEffect } from 'react';
 
 function App() {
- 
+  useEffect(() => {
+    NET({
+      el: '#vanta'
+    })
+  }, [])
+  
+
+
   return (
-    
-    <>
-      <Nav />
-      <main className="mx-3">
-        <Outlet />
-      </main>
-    </>
+    <><div className="app">
+      <div className="bg" id="vanta"></div>
+    </div><>
+        <Nav />
+        <main className="mx-3">
+          <Outlet />
+        </main>
+      </></>
   );
 }
 
